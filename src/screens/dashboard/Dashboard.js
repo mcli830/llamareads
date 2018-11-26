7292c9851d25928dae0c3fb0d602472e784bcbbe
oiioiiiio <me@oiioiii.io>
2018-11-26 15:48:33 +0900

    inbox

--- src/screens/dashboard/Dashboard.js
+++ src/screens/dashboard/Dashboard.js
@@ -24,21 +24,21 @@ const enhance = compose(
   firebaseConnect(),
   connect(({ firebase: { auth } }) => ({ auth })),
   withHandlers({
-    pushSample: props => ({ auth }) =>
+    addBook: props => ({ auth }) =>
       props.firestore.add("booksList", {
         bookFor: props.auth.uid,
-        book: "9xzHVb6LM4Cq67XUHFRF"
+        book: "9xzHVb6LM4Cq67XUHFRF",
+        inbox: true
       }),
     sendBook: props => e => {
-      console.log(e.target);
-      // props.firestore.add('booksList', { bookFor: key, book: "9xzHVb6LM4Cq67XUHFRF" })
+      props.firestore.add('booksList', { bookFor: e.target.value, book: "9xzHVb6LM4Cq67XUHFRF", inbox: true })
       // props.firestore.add('journeyList', { sender: key, notes: e.target.value})
     }
   }),
   firestoreConnect(({ auth }) => [
     {
       collection: "users"
-    }
+    },
   ]),
   connect(({ firestore }) => ({
     users: firestore.ordered.users
@@ -55,7 +55,7 @@ const enhance = compose(
 
 const Dashboard = ({
   users,
-  pushSample,
+  addBook,
   onSearchChange,
   searchVal,
   sendBook
@@ -72,15 +72,14 @@ const Dashboard = ({
             <button
               onClick={sendBook}
               key={user.id}
-              value={user.id && searchVal}
+              value={user.id}
             >
               Send Book
             </button>
           ))}
-      <button onClick={pushSample} />
+      <button onClick={addBook} />
       <input value={searchVal} onChange={onSearchChange} type="text" />
-      <ModalBase content={<ReceiveModal />} />
-      <button onClick={pushSample} />
+      {/* <ModalBase content={<ReceiveModal />} /> */}
     </div>
   );
 };
--- src/screens/dashboard/components/navbar/ListEntry.js
+++ src/screens/dashboard/components/navbar/ListEntry.js
@@ -4,18 +4,15 @@ import { compose } from "redux";
 import { withHandlers, branch, renderNothing } from "recompose";
 import { withFirestore } from "react-redux-firebase";
 
-const ListEntry = props => {
+const ListEntry = ({ book }) => {
   return (
     <div className="ListEntry">
       <div>Nov 22 2018</div>
       <div>View</div>
-      <div>Pax</div>
-      <div>Sara Pennypacker</div>
+      <div>{book.title}</div>
+      <div>{book.author}</div>
       <div>
-        A moving story of the extraordinary friendship between a boy and his
-        fox, and their epic journey to be reunited. Beautifully illustrated by
-        multi-award winner, Jon Klassen. Pax was only a kit when his family was
-        killed and he was rescued by ‘his boy’, Peter…
+        {book.excerpt}
       </div>
     </div>
   );
