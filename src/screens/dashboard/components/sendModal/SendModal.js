58160388e72ebf573955e4fe77aaf48491aae34c
Sam Huang <sam.t.huang@me.com>
2018-11-29 14:23:43 +0900

    notification dot on received complete

--- src/screens/dashboard/components/sendModal/SendModal.js
+++ src/screens/dashboard/components/sendModal/SendModal.js
@@ -20,27 +20,24 @@ import {
 //actions
 import viewModal from "../../../../functions/actions/viewModal";
 
-
 const enhance = compose(
   withFirestore,
-  connect(({view, dispatch}) => ({view, dispatch})),
+  connect(({ view, dispatch }) => ({ view, dispatch })),
   firebaseConnect(),
   connect(({ firebase: { auth } }) => ({ auth })),
   firestoreConnect(({ auth }) => [
     {
       collection: "users"
-    },
+    }
   ]),
-  connect(
-    ({ firestore }) => ({
-      users: firestore.ordered.users,
-    })
-  ),
-  withState('note', 'writeNote', ''),
-  withState('receiver', 'changeReceiver', ''),
-  withState('uid', 'uidChange', ''),
+  connect(({ firestore }) => ({
+    users: firestore.ordered.users
+  })),
+  withState("note", "writeNote", ""),
+  withState("receiver", "changeReceiver", ""),
+  withState("uid", "uidChange", ""),
   withHandlers({
-    sendBook:  props => ({ auth }) =>
+    sendBook: props => ({ auth }) =>
       props.firestore.add("userBooks", {
         sender: props.auth.uid,
         senderName: props.auth.displayName,
@@ -49,53 +46,81 @@ const enhance = compose(
         user: document.getElementById("uid").value,
         note: props.note,
         sendDate: props.firestore.FieldValue.serverTimestamp(),
-        journeyBook: {sender: props.auth.displayName, note: props.note}
+        journeyBook: { sender: props.auth.displayName, note: props.note }
       }),
     noteChange: props => event => {
-      props.writeNote(event.target.value)
+      props.writeNote(event.target.value);
     },
-    receiverChange: props => e => (
-      props.changeReceiver(e.target.value)
-    ),
-    uidChange: props => event => (
-      props.uidChange(event)
-    )
-  }),
+    receiverChange: props => e => props.changeReceiver(e.target.value),
+    uidChange: props => event => props.uidChange(event)
+  })
 );
 
-
-const SendModal = (props) => {
+const SendModal = props => {
   return (
     <div className="SendModal">
       <div className="SendForm">
         <div className="SendForm-search">
-          <input id="send-friend-input" placeholder="Type in your friend’s name" id="SendForm-search-input" value={props.receiver} onChange={(e)=>props.receiverChange(e)} />
+          <input
+            id="send-friend-input"
+            placeholder="Type in your friend’s name"
+            id="SendForm-search-input"
+            value={props.receiver}
+            onChange={e => props.receiverChange(e)}
+          />
+          {!isLoaded(props.users)
+            ? ""
+            : isEmpty(props.users)
+            ? ""
+            : props.users
+                .filter(
+                  receiver =>
+                    receiver.displayName
+                      .toLowerCase()
+                      .includes(props.receiver.toLowerCase()) &&
+                    props.receiver != ""
+                )
+                .map(user => (
+                  <div className="divUsername">
+                    <input
+                      type="hidden"
+                      key={user.id}
+                      id="uid"
+                      value={user.id}
+                    />
+                    <div className="Search-friend-results">
+                      {user.displayName}
+                    </div>
+                  </div>
+                ))[0]}
         </div>
-        {
-        !isLoaded(props.users)
-          ? ''
-          : isEmpty(props.users)
-            ? ''
-            : (
-              props.users.filter(receiver => receiver.displayName.toLowerCase().includes(props.receiver.toLowerCase()) && props.receiver != '').map((user) =>
-              <div className="divUsername">
-              <input type="hidden" key={user.id} id="uid" value={user.id}></input>
-              <p>{user.displayName}</p></div>         )[0]
-              
-            )
-        }
+
         <div className="SendForm-note">
-          <textarea id="send-note-input" placeholder="Add a short note" id="SendForm-note-input" value={props.note} onChange={(e)=>props.noteChange(e)} />
+          <textarea
+            id="send-note-input"
+            placeholder="Add a short note"
+            id="SendForm-note-input"
+            value={props.note}
+            onChange={e => props.noteChange(e)}
+          />
         </div>
       </div>
       <div className="ReceiveModal-actions mbtn-container">
-        <button className="mbtn mbtn-cancel" onClick={()=>props.dispatch(viewModal())} >Cancel</button>
-        <button className="mbtn mbtn-confirm" onClick={(e)=>{
-          if (props.note != "") {
-            props.sendBook(e);
-            props.dispatch(viewModal());
-          }
-        }}>
+        <button
+          className="mbtn mbtn-cancel"
+          onClick={() => props.dispatch(viewModal())}
+        >
+          Cancel
+        </button>
+        <button
+          className="mbtn mbtn-confirm"
+          onClick={e => {
+            if (props.note != "") {
+              props.sendBook(e);
+              props.dispatch(viewModal());
+            }
+          }}
+        >
           Send
         </button>
       </div>
